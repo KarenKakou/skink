3a398d274a1c0940bdd610ce2d6d553088982e49
karen <k.lombard.u@gmail.com>
2019-12-03 15:03:03 +0100

    feature/1:premier fichier index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$module = "acceuil";
+
+switch($module){
+
+	case "connexion":
+		break;
+	
+	case "inscription":
+		break;
+
+	case "gallerie":
+		break;
+	
+	case "acceuil":
+		break;
+
+}
+
+
+
+?>
