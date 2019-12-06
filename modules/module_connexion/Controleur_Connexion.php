b2be871561e0b3418105df8abae8358777570b52
DamienF <49200893+DamienF14@users.noreply.github.com>
2019-12-06 21:14:39 +0100

    feature/ConnexionBD:Inscription/Connexion/Deconnexion
    J'ai réalisé le module Connexion, permettant la création de toutes les fonctionnalités liés à la connexion au site

---
+++ modules/module_connexion/Controleur_Connexion.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('modules/module_connexion/Vue_Connexion.php');
+require_once('modules/module_connexion/Modele_connexion.php');
+
+    class Controleur_Connexion
+    {
+        private $vueConnexion;
+        private $modeleConnexion;
+
+        public function __construct()
+        {
+            $this->vueConnexion = new Vue_Connexion();
+            $this->modeleConnexion = new Modele_connexion();
+        }
+
+        //Methode de connexion
+        public function connect($email, $password) {
+            $this->modeleConnexion->connect($email, $password);
+        }
+
+        //Formulaire d'inscription pour le client
+        public function formInscription() {
+            $this->vueConnexion->form_InscriptionClient();
+        }
+
+        //Formulaire d'inscription pour l'admin
+        public function formInscriptionAdmin() {
+            $this->vueConnexion->form_InscriptionParAdmin();
+        }
+
+        //Ajout de l'inscripton dans la base de donnee dans la table "Compte"
+        public function ajoutinscription($nom, $prenom, $adresse, $telephone, $email, $password, $statut=1) {
+            $this->modeleConnexion->ajoutCompte($nom, $prenom, $adresse, $telephone,$email, $password, $statut);
+        }
+
+        //Formulaire permettant de se connecter
+        public function formConnexion() {
+            $this->vueConnexion->form_Connexion();
+        }
+
+        //Method pour se deconnecter
+        public function deconnect() {
+            $this->modeleConnexion->deconnect();
+        }
+    }
---
+++ modules/module_connexion/Modele_connexion.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once('modules/module_connexion/Controleur_Connexion.php');
+
+class Modele_connexion extends Connexion
+{
+    public function __construct()
+    {
+    }
+
+    //Connect teste la connection et la permet si le login et le motDePasse correspond
+    public function connect($email, $password) {
+        $selectUser = $this::$bdd->prepare('SELECT motDePasse from compte where emailCompte=?');
+        $array = array($email);
+        $selectUser->execute($array);
+        $resultUser = $selectUser->fetchAll();
+        $hashedpassword= array_shift($resultUser);
+        if(password_verify($password, $hashedpassword['motDePasse']))
+        {
+            $_SESSION['Login'] = $email;
+            echo "Vous etes connecté $email !";
+        }
+        else
+        {
+            echo "Le mot de passe ou l'identifiant sont incorrects";
+        }
+    }
+
+    //deconnect permet la deconnexion a la session
+    public function deconnect() {
+        $log = $_SESSION['Login'];
+        echo "Déconnection de $log";
+        session_destroy();
+        echo "Vous êtes déconnecté";
+    }
+
+    //Methode permettant d'ajouter un compte a la base de donnee / Statut est par défaut a 1 pour les clients
+    public function ajoutCompte($nom, $prenom, $adresse, $telephone, $email, $password, $statut = 1) {
+        if($this->verifEmail($email)) {
+            $hashpassword = password_hash($password, PASSWORD_DEFAULT);
+            $nbStatut = 0;
+            switch ($statut) {
+                case 'Client':
+                    $nbStatut = 1;
+                    break;
+                case 'Tatoueur' :
+                    $nbStatut = 2;
+                    break;
+                case 'Admin':
+                    $nbStatut = 3;
+                    break;
+                default:
+                    $nbStatut = 1;
+            }
+
+            $insertPrepare = $this::$bdd->prepare('INSERT into compte values(DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?)');
+            $array = array($nom, $prenom, $adresse, $telephone, $hashpassword, $email, NULL, $nbStatut);
+            if ($insertPrepare->execute($array)) {
+                echo "L'utilisateur $nom $prenom a bien été enregistré au statut de $statut";
+            } else {
+                echo "Il y a eu un problème avec l'inscription de l'utilisateur $nom $prenom";
+            }
+        }else {
+            echo "L'adresse email n'est pas valide";
+        }
+    }
+
+    //Method qui verifie si l'adresse mail contient un @
+    private function verifEmail($email) {
+        if(strpos($email, "@") === false) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+}
