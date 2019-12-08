d9329a5fb64f0495d4128efdda89453b6e02a423
KarenKakou <49200814+KarenKakou@users.noreply.github.com>
2019-12-08 14:12:23 +0100

    Add files via upload
    accueil en mvc + nav + carousel

---
+++ modules/acceuil/cont_accueil.php
@@ -0,0 +1,23 @@
+<?php
+
+//require_once "modules/acceuil/modele_accueil.php";
+require_once "modules/acceuil/vue_accueil.php";
+
+class ContAccueil{
+
+	//private $modele;
+	private $vue;
+
+	public function __construct (){
+		//$this->modele = new ModelAccueil();
+		$this->vue = new VueAccueil();
+	}
+
+	public function accueil(){
+		$this->vue->carousel();
+		$this->vue->pied();
+	}
+
+}
+
+?>
