573248c179497da4168cb830c851e2c4df06ee5d
DamienF14 <49200893+DamienF14@users.noreply.github.com>
2020-01-15 16:19:11 +0100

    feature/ChangeMessageConnexion&ButtonGalerie

--- connexion.php
+++ connexion.php
@@ -1,13 +1,14 @@
 <?php
 
 class Connexion {
-    protected static $bdd;
+
+    public static $bdd;
 
     public static function initConnexion() {
     	//Connexion de ma maison
-		$dns = 'mysql:host=localhost; dbname=skink';
+		$dns = 'mysql:host=localhost; dbname=skinkv3';
 		self::$bdd = new PDO($dns, 'root', '');
-		
+
 		/* connexion iut
 		$dns = "mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201638" ;
 		$user = "dutinfopw201638";
