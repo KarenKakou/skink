91e96be7263beb3635ad81e516f96aceba8cab5d
DamienF14 <49200893+DamienF14@users.noreply.github.com>
2020-01-18 22:55:02 +0100

    feature/htmlSpecial

--- modules/module_messagerie/Modele_Messagerie.php
+++ modules/module_messagerie/Modele_Messagerie.php
@@ -52,9 +52,7 @@ class Modele_Messagerie  extends Connexion {
         $array = array($_SESSION['idCompte'],$compteTatoueur);
         $listConv->execute($array);
         $listConversation = $listConv->fetchAll();
-        var_dump($listConversation);
         if(empty($listConversation)) {
-            echo"Mon dieu";
             $insertPrepare = $this::$bdd->prepare('INSERT into CONVERSATION values(DEFAULT)');
             if ($insertPrepare->execute()) {
                 $idConv = $this::$bdd->lastInsertId();
@@ -71,13 +69,9 @@ class Modele_Messagerie  extends Connexion {
 
     //Methode permettant d'envoyer un nouveau message
     public function nouveauMessage($corps, $idConv, $idCompteEmetteur, $idCompteReceveur) {
-        if(isset($_FILES['MessageImage'])) {
-            $pieceJointe = $this->uploadImageMessagerie();
-        }else {
-            $pieceJointe = null;
-        }
+
         $insertMessagePrepare = $this::$bdd->prepare('INSERT into MESSAGE values(DEFAULT, ?, ?, ?, ?, ?)');
-        $array = array($corps, $pieceJointe, $idConv, $idCompteEmetteur, $idCompteReceveur);
+        $array = array($corps, null, $idConv, $idCompteEmetteur, $idCompteReceveur);
         if(!$insertMessagePrepare->execute($array)) {
             echo "Il y a eu un problème avec l'envoie du message";
         }
@@ -104,33 +98,4 @@ class Modele_Messagerie  extends Connexion {
             return $premierMess['idCompte'];
     }
 
-    public function uploadImageMessagerie() {
-
-        $image_name=$_FILES['MessageImage']['name'];
-        $file_size =$_FILES['MessageImage']['size'];
-        $explode = explode('.',$_FILES['MessageImage']['name']);
-        $file_ext=strtolower(end($explode));
-
-        $extensions= array("jpeg","jpg","png");
-
-            if(in_array($file_ext,$extensions)=== false){
-                //s$errors[]="extension non prise en charge";
-            }
-
-            if($file_size > 2097152){
-                $errors[]='fichier trop lourd (doit être inférieur à 2 MB)';
-            }
-
-            $temp = explode(".", $image_name);
-            $imagepath="images/images_messagerie/".$image_name;
-
-            if(empty($errors)==true){
-                move_uploaded_file($_FILES["MessageImage"]["tmp_name"],$imagepath);
-                echo "image ajoutée";
-            }
-            else{
-            print_r($errors);
-        }
-        return $image_name;
-    }
 }
