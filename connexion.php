f1948e107e6fe95be564e0583aff5094e4627873
karen <k.lombard.u@gmail.com>
2020-03-03 09:21:31 +0100

    test

--- connexion.php
+++ connexion.php
@@ -4,8 +4,10 @@ class Connexion {
     protected static $bdd;
 
     public static function initConnexion() {
-		$dns = 'mysql:host=localhost; dbname=skink';
-		self::$bdd = new PDO($dns, 'root', '');
+		$dns = "mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201638" ;
+		$user = "dutinfopw201638";
+		$password = "gagyqapu";
+		self::$bdd = new PDO($dns, $user, $password);
 	}
 
 
